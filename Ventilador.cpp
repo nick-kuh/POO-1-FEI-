8df356e673c5a09ae4919fc81d4fce32059b318b
nick-kuh <142516148+nick-kuh@users.noreply.github.com>
2024-10-04 21:48:15 +0000

    Ventilador

--- Ventilador.cpp
+++ Ventilador.cpp
@@ -28,25 +28,25 @@ class Ventilador: public Atuador{
       if (ligado) {
         if (valor < 23){
           this->setVelocidade(0);
-          cout << "Ventilador Desligado" << endl;
+          // cout << "Ventilador Desligado" << endl;
           return make_pair(this->desligar(), this->temperatura);
         }
         else{
           this->setVelocidade(1);
           this->temperatura -= 1;
-          cout << "Ventilador Ligado" << endl;
+          // cout << "Ventilador Ligado" << endl;
           return make_pair(this->ligar(), this->temperatura);
         }
       }
       else{
         if (temperatura > 25){
           this->setVelocidade(1);
-          cout << "Ventilador Ligado" << endl;
+          // cout << "Ventilador Ligado" << endl;
           return make_pair(this->ligar(), this->temperatura);
         }
         else{
           this->setVelocidade(0);
-          cout << "Ventilador Desligado" << endl;
+          // cout << "Ventilador Desligado" << endl;
           return make_pair(this->desligar(), this->temperatura);
         }
       }
@@ -63,3 +63,10 @@ class Ventilador: public Atuador{
   }
 
 };
+
+
+
+
+
+
+
